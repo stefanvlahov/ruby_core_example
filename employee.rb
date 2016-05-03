f4ce2cb48f6f842a4d072d38ec9e0603ae1491cc
stefanvlahov <stefan.vlahov5@gmail.com>
2016-05-02 21:18:45 -0500

    Initial

---
+++ employee.rb
@@ -0,0 +1,5 @@
+employee1 = ["John", "Smith", 60000, true]
+employee2 = ["Jane", "Doe", 70000, true]
+
+puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+puts "#{employee1[0]} #{employee1[1]} makes #{employee1[2]} a year."
