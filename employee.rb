46a10c0151586200511e6c1462266bdf1244d5df
stefanvlahov <stefan.vlahov5@gmail.com>
2016-05-03 20:25:01 -0500

    Added Class and other examples

--- employee.rb
+++ employee.rb
@@ -3,27 +3,49 @@
 #
 # puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
 # puts "#{employee1[0]} #{employee1[1]} makes #{employee1[2]} a year."
-
-# employee1 = { "first_name" => "John", "last_name" => "Smith", "salary" => 60000, "active" => true}
-# employee2 = { "first_name" => "Jane", "last_name" => "Doe", "salary" => 70000, "active" => true}
+#
+# employee1 = { first_name: "John", last_name: "Smith", salary: 60000, active: true}
+# employee2 = { first_name: "Jane", last_name: "Doe", salary: 70000, active: true}
 #
 # puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
 # puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
-
-puts
+# p employee1[:first_name]
+# puts
 
 class Employee
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+
+  attr_reader :active, :salary
+  # attr_writer
+  attr_accessor :first_name
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    if input_options[:active]
+      @active = input_options[:active]
+    else
+      @active = true
+    end
   end
 
   def print_info
     puts "#{@first_name} #{@last_name} makes #{@salary} a year."
   end
+
+  def give_annual_raise
+    @salary *=  1.05
+  end
+
+  # def first_name=(input_first_name)
+  #   @first_name = input_first_name
+  # end
+
 end
 
-employee1 = Employee.new("John", "Smith", 60000, true)
-employee1.print_info
+employee1 = Employee.new({ first_name: "John", last_name: "Smith", salary: 60000, active: true})
+employee2 = Employee.new({ first_name: "Jane", last_name: "Doe", salary: 70000, active: true})
+p employee1
+# employee1.change_first_name=("Bill")
+# employee1.print_info
+# p employee2.active
